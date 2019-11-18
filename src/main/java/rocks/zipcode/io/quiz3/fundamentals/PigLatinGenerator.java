dcbe7fdc4a07fef1ae57bf1e5f87ace5fc26c3d1
Amendoza20 <20.angel.mendoza@gmail.com>
2019-11-18 16:33:29 -0500

    addedway omesay odecay

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -1,11 +1,19 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+
+import static rocks.zipcode.io.quiz3.fundamentals.VowelUtils.isVowel;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-
-        return null;
+        StringBuilder pigString = new StringBuilder();
+        if(isVowel(str.charAt(0))){
+            pigString.append(str).append("way");
+        }else if(!isVowel(str.charAt(0))){
+            pigString.append(str.substring(1,str.length())).append(str.charAt(0)).append("ay");
+        }
+        return pigString.toString();
     }
 }
